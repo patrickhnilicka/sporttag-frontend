37da0667ee73d6d55f1e5ad334021df8c6bb9809
patrickhnilicka <patrick.hnilicka@hsr.ch>
2024-03-03 13:54:11 +0100

    some minor changes

--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 import StudentShow from '../components/StudentShow.vue';
 import StudentEdit from '../components/StudentEdit.vue';
@@ -15,7 +14,7 @@ export default defineComponent({
     return {
       students: [] as Student[],
       prefix: '',
-      student: { id: -1, vorname: '', nachname: '', geschlecht: '', geburtsdatum: new Date()} as Student,
+      student: { id: -1, vorname: '', nachname: '', geschlecht: '', geburtsdatum: new Date() } as Student,
       isEdit: false
     }
   },
@@ -24,8 +23,7 @@ export default defineComponent({
   },
   computed: {
     filteredStudents() {
-      return this.students.filter((n) =>
-        n.nachname.toLowerCase().startsWith(this.prefix.toLowerCase())
+      return this.students.filter((n) => { return n.nachname.toLowerCase().startsWith(this.prefix.toLowerCase()) || n.vorname.toLowerCase().startsWith(this.prefix.toLowerCase()) }
       )
     }
   },
@@ -43,7 +41,7 @@ export default defineComponent({
         console.log(error)
       }
     },
-    async saveStudent(student:Student) {
+    async saveStudent(student: Student) {
       try {
         await fetch(url + "/student", {
           method: "POST",
@@ -60,22 +58,27 @@ export default defineComponent({
       this.student = student
     },
     create() {
-      this.student = { id: -1, vorname: '', nachname: '', geschlecht: '', geburtsdatum: new Date()}
+      this.student = { id: -1, vorname: '', nachname: '', geschlecht: '', geburtsdatum: new Date() }
       this.isEdit = true
     },
     edit() {
       this.isEdit = true
     },
-    del() {
-      if (this.student.id != -1) {
-        this.students = this.students.filter(p => p.id != this.student.id)
-        this.student = this.students[0]
+    async del(id: Number) {
+      try {
+        await fetch(url + "/student/" + id, {
+          method: "DELETE",
+        })
       }
+      catch (error) {
+        console.log(error)
+      }
+      this.getStudents()
     },
     hasValidInput() {
       return this.student.vorname.trim() && this.student.nachname.trim()
     },
-    updateStudent(uStudent:Student) {
+    updateStudent(uStudent: Student) {
       console.log("Update Student:")
       console.log(uStudent)
       this.saveStudent(uStudent)
@@ -84,7 +87,7 @@ export default defineComponent({
     abortEdit() {
       this.isEdit = false
     },
-    getDisplayName(student:Student) {
+    getDisplayName(student: Student) {
       return student.vorname + ' ' + student.nachname
     }
   }
@@ -95,25 +98,50 @@ export default defineComponent({
   <div class="container">
     <div id="studentlist">
       <div>
-        <input v-model="prefix" placeholder="Filter prefix" :disabled="isEdit">
+        <input v-model="prefix" placeholder="Vor- oder Nachnamen filtern" :disabled="isEdit">
       </div>
       <div>
+        <table width="100%">
+          <tr align="left">
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Geschlecht</th>
+            <th>Geburtstag</th>
+            <th>Sportklasse</th>
+            <th>Bearbeiten</th>
+          </tr>
+          <tr v-for="s in filteredStudents" v-bind:key="s.id.toString">
+            <td>{{ s.vorname }}</td>
+            <td>{{ s.nachname }}</td>
+            <td>{{ s.geschlecht }}</td>
+            <td>{{ s.geburtsdatum }}</td>
+            <td>Sportklasse</td>
+            <td><span class="material-symbols-outlined">edit</span><button @click="del(s.id)" v-if="!isEdit"><span class="material-symbols-outlined">delete</span></button></td>
+          </tr>
+        </table>
+      </div>
+      <!--div>
         <select size="5" v-model="student" :disabled="isEdit">
           <option v-for="student in filteredStudents" :key="student.id.toString" :value="student">{{ getDisplayName(student) }}</option>
         </select>
-      </div>
+      </div-->
     </div>
     <div id="student">
       <StudentShow v-if="!isEdit" :student="student" @edit="edit" />
       <StudentEdit v-else :student="student" @save-student="updateStudent" @abort-edit="abortEdit" />
       <div class="buttons">
         <button @click="create" v-if="!isEdit">New</button>
-        <button @click="del" v-if="!isEdit">Delete</button>
+        <!--button @click="del({{ s.id }})" v-if="!isEdit">Delete</button-->
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0');
+tr:nth-child(even) {background-color: #f2f2f2;}
+th {
+  background-color: rgb(35 158 221);
+  color: white;
+}
 </style>
